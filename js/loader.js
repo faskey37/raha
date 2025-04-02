fa18f4c615872b548ce8196e02ead6ce0b915672
khantauqeer272@gmail.com <khantauqeer272@gmail.com>
2025-04-02 13:08:27 +0530

    wed

--- js/loader.js
+++ js/loader.js
@@ -2,6 +2,9 @@ document.addEventListener("DOMContentLoaded", function () {
     let counter = document.querySelector(".progress-counter");
     let loader = document.querySelector(".custom-loader");
 
+    // First ensure body has no scroll
+    document.body.style.overflow = 'hidden';
+    
     let progress = 0;
 
     let interval = setInterval(() => {
@@ -12,6 +15,8 @@ document.addEventListener("DOMContentLoaded", function () {
             clearInterval(interval);
             setTimeout(() => {
                 loader.classList.add("hidden");
+                // Restore body scroll when loader is hidden
+                document.body.style.overflow = '';
             }, 500);
         }
     }, 200);
