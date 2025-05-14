f222a6dcf0949339f4b2c9b00136081b68c0ca48
khantauqeer272@gmail.com <khantauqeer272@gmail.com>
2025-05-14 10:19:09 +0530

    Update home.js

--- js/home.js
+++ js/home.js
@@ -303,21 +303,3 @@ async function sendMessage() {
     responseDiv.appendChild(errorMsg);
   }
 }
-particlesJS("particles-js", {
-  "particles": {
-    "number": {"value": 80, "density": {"enable": true, "value_area": 800}},
-    "color": {"value": "#4dd0e1"},
-    "shape": {"type": "circle"},
-    "opacity": {"value": 0.5, "random": true},
-    "size": {"value": 3, "random": true},
-    "line_linked": {"enable": true, "distance": 150, "color": "#4dd0e1", "opacity": 0.4, "width": 1},
-    "move": {"enable": true, "speed": 2, "direction": "none", "random": true, "straight": false, "out_mode": "out"}
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {"enable": true, "mode": "grab"},
-      "onclick": {"enable": true, "mode": "push"}
-    }
-  }
-});
